f51d69e10e0916981d4799de962dc8e0db49a9fc
thangltt <thanglt@tinhvan.com>
2019-08-20 18:10:52 +0700

    #thanglt_fix login gen token

--- src/main/java/com/tvo/controller/LoginController.java
+++ src/main/java/com/tvo/controller/LoginController.java
@@ -6,6 +6,8 @@ package com.tvo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +36,7 @@ public class LoginController {
 			@ApiResponse(code = 200, message = "Successssss", response = User.class),
 			@ApiResponse(code = 422, message = "Student not found"),
 			@ApiResponse(code = 417, message = "Exception failed") })
-	@GetMapping(value = "/login")
-	public void loginUrl(@RequestParam("username") String username, @RequestParam("password") String password) {
+	@PostMapping(value = "/login")
+	public void loginUrl(@RequestBody User user) {
 	}
 }
