08a90e400b736109b8237b6167849489f9a20676
Dinh Cong Ngoc <ngocdc@tinhvan.com>
2019-08-22 16:27:45 +0700

    coding task-1-2-createApi-getUserDetail-and-deleteUser

--- src/main/java/com/tvo/model/User.java
+++ src/main/java/com/tvo/model/User.java
@@ -5,14 +5,7 @@ package com.tvo.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -31,7 +24,8 @@ public class User extends AbstractEntity implements Serializable {
 	private static final long serialVersionUID = 8922785714798336673L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AI_CMS_USER_SQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_ID_SQ_GENERATOR")
+	@SequenceGenerator(sequenceName = "AI_CMS_USER_SQ", allocationSize = 1, name = "USER_ID_SQ_GENERATOR")
 	@Column(name = "USER_ID", nullable = false)
 	private Long userId;
 
--- src/main/java/com/tvo/service/AuditorAwareImpl.java
+++ src/main/java/com/tvo/service/AuditorAwareImpl.java
@@ -17,24 +17,24 @@ import com.tvo.model.User;
  * @author Ace
  *
  */
-public class AuditorAwareImpl implements AuditorAware<Long> {
-
-	@Autowired
-	AppUserDAO appUserDAO;
-	
-	@Override
-	public Optional<Long> getCurrentAuditor() {
-		User user = new User(); 
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if (principal instanceof UserDetails) {
-			String username = ((UserDetails) principal).getUsername();
-			 user = appUserDAO.findByUserName(username);
-			 return  Optional.ofNullable(user.getUserId());
-		} else {
-			String username = principal.toString();
-			 user = appUserDAO.findByUserName(username);
-			 return  Optional.ofNullable(user.getUserId());
-		}
-	}
+public class AuditorAwareImpl {
+//
+//	@Autowired
+//	AppUserDAO appUserDAO;
+//
+//	@Override
+//	public Optional<Long> getCurrentAuditor() {
+//		User user = new User();
+//		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//		if (principal instanceof UserDetails) {
+//			String username = ((UserDetails) principal).getUsername();
+//			 user = appUserDAO.findByUserName(username);
+//			 return  Optional.ofNullable(user.getUserId());
+//		} else {
+//			String username = principal.toString();
+//			 user = appUserDAO.findByUserName(username);
+//			 return  Optional.ofNullable(user.getUserId());
+//		}
+//	}
 
 }
