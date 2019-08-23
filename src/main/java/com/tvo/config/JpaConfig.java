a7832fe278c91ed0b989d5c1a928e155504a741f
Dinh Cong Ngoc <ngocdc@tinhvan.com>
2019-08-23 09:24:33 +0700

    coding task-1-2-fixAuditorAware

--- src/main/java/com/tvo/config/JpaConfig.java
+++ src/main/java/com/tvo/config/JpaConfig.java
@@ -3,22 +3,22 @@
  */
 package com.tvo.config;
 
+import com.tvo.model.User;
+import com.tvo.service.AuditorAwareImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.tvo.service.AuditorAwareImpl;
-
 /**
  * @author Ace
  *
  */
-//@Configuration
-//@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class JpaConfig {
-//	@Bean
-//	public AuditorAware<Long> auditorAware() {
-//		return new AuditorAwareImpl();
-//	}
+	@Bean
+	public AuditorAware<User> auditorAware() {
+		return new AuditorAwareImpl();
+	}
 }
--- src/main/java/com/tvo/service/AuditorAwareImpl.java
+++ src/main/java/com/tvo/service/AuditorAwareImpl.java
@@ -3,38 +3,37 @@
  */
 package com.tvo.service;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import com.tvo.dao.AppUserDAO;
 import com.tvo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * @author Ace
  *
  */
-public class AuditorAwareImpl {
-//
-//	@Autowired
-//	AppUserDAO appUserDAO;
-//
-//	@Override
-//	public Optional<Long> getCurrentAuditor() {
-//		User user = new User();
-//		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//		if (principal instanceof UserDetails) {
-//			String username = ((UserDetails) principal).getUsername();
-//			 user = appUserDAO.findByUserName(username);
-//			 return  Optional.ofNullable(user.getUserId());
-//		} else {
-//			String username = principal.toString();
-//			 user = appUserDAO.findByUserName(username);
-//			 return  Optional.ofNullable(user.getUserId());
-//		}
-//	}
+public class AuditorAwareImpl implements AuditorAware<User>{
+
+	@Autowired
+	AppUserDAO appUserDAO;
+
+	@Override
+    @Transactional
+	public Optional<User> getCurrentAuditor() {
+        User auditor = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof User) {
+                auditor = (User) principal;
+            }
+        }
+        return Optional.ofNullable(auditor);
+	}
 
 }
