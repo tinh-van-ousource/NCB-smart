3ac09f25f2bb7fcdc4b84454415e0ffc57d366b6
Dinh Cong Ngoc <ngocdc@tinhvan.com>
2019-09-13 11:25:04 +0700

    coding hotfix/par-card-search

---
+++ src/main/java/com/tvo/controllerDto/ParCardSearchReqDto.java
@@ -0,0 +1,12 @@
+package com.tvo.controllerDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ParCardSearchReqDto {
+	private String prdcode;
+	private String product;
+	private String status;
+}
