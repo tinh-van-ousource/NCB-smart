d652ece1b2e1d4085284c9a44c4224e051088c38
Dinh Cong Ngoc <ngocdc@tinhvan.com>
2019-09-16 09:58:54 +0700

    coding fixbug/apiProvider

---
+++ src/main/java/com/tvo/request/ProviderUpdateReqDto.java
@@ -0,0 +1,23 @@
+package com.tvo.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+public class ProviderUpdateReqDto {
+    @NotNull
+    private Long id;
+
+    private String providerCode;
+
+    private String providerName;
+
+    private String serviceCode;
+
+    private String partner;
+
+    private String status;
+}
