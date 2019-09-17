a58b949357e7dbfef9a6ac4076dcb896a3d2f871
QuachQuangTuan <tuanboomcn4ptit@gmail.com>
2019-09-17 16:06:06 +0700

    fix phân biệt chữ hoa

--- src/main/java/com/tvo/service/FunctionServiceImpl.java
+++ src/main/java/com/tvo/service/FunctionServiceImpl.java
@@ -71,18 +71,19 @@ public class FunctionServiceImpl implements FunctionService {
 
 		if (resource.getPrdName() != null
 				&& !org.apache.commons.lang3.StringUtils.isEmpty(resource.getPrdName().trim())) {
-			predicates.add(cb.and(cb.equal(rootPersist.<String>get("prdname"), resource.getPrdName())));
+			predicates.add(cb.and(cb.equal(cb.upper(rootPersist.<String>get("prdName")), resource.getPrdName().toUpperCase())));
 		}
 
 		if (resource.getTranType() != null
 				&& !org.apache.commons.lang3.StringUtils.isEmpty(resource.getTranType().trim())) {
-			predicates.add(cb.and(cb.equal(rootPersist.<String>get("tranType"), resource.getTranType())));
+			predicates.add(cb.and(cb.equal(cb.upper(rootPersist.<String>get("prdName")), resource.getTranType().toUpperCase())));
 		}
 
 		if (resource.getTypeId() != null
 				&& !org.apache.commons.lang3.StringUtils.isEmpty(resource.getTypeId().trim())) {
-			predicates.add(cb.and(cb.equal(rootPersist.<String>get("typeId"), resource.getTypeId())));
+			predicates.add(cb.and(cb.equal(cb.upper(rootPersist.<String>get("prdName")), resource.getTypeId().toUpperCase())));
 		}
+
 		
 
 		Object[] results = new Object[2];
