79ad90261ffd049c6b2768ad6ae77046deb71ccc
QuachQuangTuan <tuanboomcn4ptit@gmail.com>
2019-10-02 11:28:03 +0700

    fix_FTP_v2

--- src/main/java/com/tvo/controller/FileController.java
+++ src/main/java/com/tvo/controller/FileController.java
@@ -84,52 +84,29 @@ public class FileController {
         return new ResponeData<>(AppConstant.SYSTEM_ERROR_CODE, AppConstant.SYSTEM_ERROR_MESSAGE, null);
     }
     public void uploadFileViaFTP(MultipartFile fileUpload) {
-		String server = "http://115.146.124.153:8080";
+    	String server = "10.1.62.33";
 		String user = "cmsuat";
 		String pass = "123@123aA";
-
+		String fileStorePath = "/CMSBanner/test.jpg";
 		FTPClient ftpClient = new FTPClient();
 		try {
-
 			ftpClient.connect(server);
 			ftpClient.login(user, pass);
 			ftpClient.enterLocalPassiveMode();
-
 			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-
-		
-
-			String fileStorePath = "/banner_mobile/test.jpg";
-			InputStream inputStream = fileUpload.getInputStream();
-
-			System.out.println("Start uploading first file");
-			boolean done = ftpClient.storeFile(fileStorePath, inputStream);
-			inputStream.close();
-			if (done) {
-				System.out.println("The first file is uploaded successfully.");
+			
+			if (ftpClient.changeWorkingDirectory(CMSBanner)) {
+				InputStream inputStream = fileUpload.getInputStream();
+			    System.out.println("Start uploading first file");
+			    boolean done = ftpClient.storeFile(fileStorePath, inputStream);
+			    inputStream.close();
+			    if (done) {
+				   System.out.println("The first file is uploaded successfully.");
+			    }
+			}else {
+			   System.out.println("CMSBanner derectory is not existed.");
 			}
-
-//			// APPROACH #2: uploads second file using an OutputStream
-//			File secondLocalFile = new File("E:/Test/Report.doc");
-//			String secondRemoteFile = "test/Report.doc";
-//			inputStream = new FileInputStream(secondLocalFile);
-//
-//			System.out.println("Start uploading second file");
-//			OutputStream outputStream = ftpClient.storeFileStream(secondRemoteFile);
-//			byte[] bytesIn = new byte[4096];
-//			int read = 0;
-//
-//			while ((read = inputStream.read(bytesIn)) != -1) {
-//				outputStream.write(bytesIn, 0, read);
-//			}
-//			inputStream.close();
-//			outputStream.close();
-//
-//			boolean completed = ftpClient.completePendingCommand();
-//			if (completed) {
-//				System.out.println("The second file is uploaded successfully.");
-//			}
-
+		
 		} catch (IOException ex) {
 			System.out.println("Error: " + ex.getMessage());
 			ex.printStackTrace();
