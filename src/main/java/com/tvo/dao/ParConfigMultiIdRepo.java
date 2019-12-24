c0ae1bc14ae4af8ee7b3c79a90f0432b2e1ed7a9
Nguyen Xuan Nam <namnx1@tinhvan.com>
2019-12-24 17:35:36 +0700

    Don't use ParConfigCompositeKey

--- src/main/java/com/tvo/dao/ParConfigMultiIdRepo.java
+++ src/main/java/com/tvo/dao/ParConfigMultiIdRepo.java
@@ -13,15 +13,15 @@ import com.tvo.model.ParConfigMultiIdEntity;
 @Repository
 public interface ParConfigMultiIdRepo extends JpaRepository<ParConfigMultiIdEntity, ParConfigCompositeKey> {
 
-    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.key.param = :param ORDER BY pce.value")
+    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.param = :param ORDER BY pce.value")
     List<ParConfigMultiIdEntity> findByParam(@Param("param") String param);
 
-    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.key.param = :param AND pce.key.code = :code ")
+    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.param = :param AND pce.code = :code ")
     ParConfigMultiIdEntity findByParamAndCode(@Param("param") String param, @Param("code") String code);
 
-    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.key.param NOT IN :param ORDER BY pce.value")
+    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.param NOT IN :param ORDER BY pce.value")
     List<ParConfigMultiIdEntity> findOtherParam(@Param("param") List<String> param);
     
-    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.key.code = :code")
+    @Query("SELECT pce FROM ParConfigMultiIdEntity pce WHERE pce.code = :code")
     ParConfigMultiIdEntity findByCode(@Param("code") String code);
 }
